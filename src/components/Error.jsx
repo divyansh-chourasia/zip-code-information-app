9ec2d2b8ec418bbe6ae04dda56dedd5677a36378
Divyansh Chourasia <110674394+divyansh-chourasia@users.noreply.github.com>
2023-11-03 11:36:21 +0530

    Delete src directory

--- src/components/Error.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Error = ({ message }) => {
-  return <div className="error">{message}</div>;
-};
-
-export default Error;
--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Search from "./Search";
-import DataDisplay from "./DataDisplay";
-import Error from "./Error";
-
-export{
-    Search,
-    DataDisplay,
-    Error
-}
